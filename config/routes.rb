b7f079cfb61907df20da2d5b73796b11d5696604
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-03-10 11:13:59 -0800

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :workouts, only: [:index, :create]
-      resources :category, only: [:index]
+      resources :categories, only: [:index]
 
     end 
   end 
